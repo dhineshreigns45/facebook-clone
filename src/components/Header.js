2dc327075df12defb8194714a9b2510549abddf5
dhineshreigns45 <dhineshkumar111202@gmail.com>
2023-10-12 19:54:25 +0530

    fb-clone

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='p-3'>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook-Logo" 
+            width={35}
+            height={35}
+            />
+        </div>
+    </div>
+  )
+}
